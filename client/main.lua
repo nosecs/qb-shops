1e36c10354dfa8ba166ba6f36dff63efc1cda50e
MrJayGamerTTV <mr.j.gamer98@gmail.com>
2022-01-24 15:51:40 +0200

    Made shops QB-Target compatible

--- client/main.lua
+++ client/main.lua
@@ -20,6 +20,7 @@ local function SetupItems(shop)
     return items
 end
 
+--[[
 local function DrawText3Ds(x, y, z, text)
     SetTextScale(0.35, 0.35)
     SetTextFont(4)
@@ -34,6 +35,7 @@ local function DrawText3Ds(x, y, z, text)
     DrawRect(0.0, 0.0+0.0125, 0.017+ factor, 0.03, 0, 0, 0, 75)
     ClearDrawOrigin()
 end
+--]]
 
 -- Events
 
@@ -53,9 +55,59 @@ RegisterNetEvent('qb-shops:client:RestockShopItems', function(shop, amount)
     end
 end)
 
+-- Setup qb-target shop event?
+
+RegisterNetEvent('qb-shops:marketshop', function()
+    for shop, data in pairs(Config.Locations) do
+        local position = data["coords"]
+        local products = data["products"]
+        for _, loc in pairs(position) do
+            local dist = #(GetEntityCoords(PlayerPedId()) - vector3(loc["x"], loc["y"], loc["z"]))
+            if dist < 3 then
+                local ShopItems = {}
+                ShopItems.items = {}
+                local callback = promise.new()
+                QBCore.Functions.TriggerCallback('qb-shops:server:getLicenseStatus', function(result)
+                    ShopItems.label = Config.Locations[shop]["label"]
+                    if Config.Locations[shop].type == "weapon" then
+                        if result then
+                            ShopItems.items = SetupItems(shop)
+                        else
+                            for i = 1, #products do
+                                if not products[i].requiredJob then
+                                    if not products[i].requiresLicense then
+                                        table.insert(ShopItems.items, products[i])
+                                    end
+                                else
+                                    for i2 = 1, #products[i].requiredJob do
+                                        if QBCore.Functions.GetPlayerData().job.name == products[i].requiredJob[i2] and not products[i].requiresLicense then
+                                            table.insert(ShopItems.items, products[i])
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                    else
+                        ShopItems.items = SetupItems(shop)
+                    end
+                    for k, v in pairs(ShopItems.items) do
+                        ShopItems.items[k].slot = k
+                    end
+                    ShopItems.slots = 30
+                    TriggerServerEvent("inventory:server:OpenInventory", "shop", "Itemshop_"..shop, ShopItems)
+                    callback:resolve(true)
+                end)
+
+                Citizen.Await(callback)
+                break
+            end
+        end
+    end
+end)
+
 -- Threads
 
-CreateThread(function()
+Citizen.CreateThread(function()
     while true do
         local InRange = false
         local PlayerPed = PlayerPedId()
@@ -63,52 +115,20 @@ CreateThread(function()
 
         for shop, _ in pairs(Config.Locations) do
             local position = Config.Locations[shop]["coords"]
-            local products = Config.Locations[shop].products
             for _, loc in pairs(position) do
                 local dist = #(PlayerPos - vector3(loc["x"], loc["y"], loc["z"]))
                 if dist < 10 then
                     InRange = true
-                    DrawMarker(2, loc["x"], loc["y"], loc["z"], 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.2, 0.1, 255, 255, 255, 155, 0, 0, 0, 1, 0, 0, 0)
+                    -- DrawMarker(2, loc["x"], loc["y"], loc["z"], 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.2, 0.1, 255, 255, 255, 155, 0, 0, 0, 1, 0, 0, 0) -- removes in game UI icon
                     if dist < 1 then
-                        DrawText3Ds(loc["x"], loc["y"], loc["z"] + 0.15, Lang:t("info.interact"))
+                        -- DrawText3Ds(loc["x"], loc["y"], loc["z"] + 0.15, '~g~E~w~ - Shop') -- removes in game UI icon
                         if IsControlJustPressed(0, 38) then -- E
+                SetWeaponSeries()
                             local ShopItems = {}
-                            ShopItems.items = {}
-                            QBCore.Functions.TriggerCallback("qb-shops:server:getLicenseStatus", function(hasLicense, hasLicenseItem)
-                                ShopItems.label = Config.Locations[shop]["label"]
-                                if Config.Locations[shop].products == Config.Products["weapons"] then
-                                    if hasLicense and hasLicenseItem then
-                                        ShopItems.items = SetupItems(shop)
-                                        QBCore.Functions.Notify(Lang:t("success.dealer_verify"), "success")
-                                        Wait(500)
-                                    else
-                                        for i = 1, #products do
-                                            if not products[i].requiredJob then
-                                                if not products[i].requiresLicense then
-                                                    ShopItems.items[#ShopItems.items+1] = products[i]
-                                                end
-                                            else
-                                                for i2 = 1, #products[i].requiredJob do
-                                                    if QBCore.Functions.GetPlayerData().job.name == products[i].requiredJob[i2] and not products[i].requiresLicense then
-                                                        ShopItems.items[#ShopItems.items+1] = products[i]
-                                                    end
-                                                end
-                                            end
-                                        end
-                                        QBCore.Functions.Notify(Lang:t("error.dealer_decline"), "error")
-                                        Wait(500)
-                                        QBCore.Functions.Notify(Lang:t("error.talk_cop"), "error")
-                                        Wait(1000)
-                                    end
-                                else
-                                    ShopItems.items = SetupItems(shop)
-                                end
-                                for k, v in pairs(ShopItems.items) do
-                                    ShopItems.items[k].slot = k
-                                end
-                                ShopItems.slots = 30
-                                TriggerServerEvent("inventory:server:OpenInventory", "shop", "Itemshop_"..shop, ShopItems)
-                            end)
+                            ShopItems.label = Config.Locations[shop]["label"]
+                            ShopItems.items = Config.Locations[shop]["products"]
+                            ShopItems.slots = 30
+                            TriggerServerEvent("inventory:server:OpenInventory", "shop", "Itemshop_"..shop, ShopItems)
                         end
                     end
                 end
@@ -116,26 +136,54 @@ CreateThread(function()
         end
 
         if not InRange then
-            Wait(5000)
+            Citizen.Wait(5000)
         end
-        Wait(5)
+        Citizen.Wait(5)
     end
 end)
 
 CreateThread(function()
-    for store, _ in pairs(Config.Locations) do
-        if Config.Locations[store]["showblip"] then
-            for i = 1, #Config.Locations[store]["coords"] do
-                StoreBlip = AddBlipForCoord(Config.Locations[store]["coords"][i]["x"], Config.Locations[store]["coords"][i]["y"], Config.Locations[store]["coords"][i]["z"])
-                SetBlipColour(StoreBlip, 0)
-                SetBlipSprite(StoreBlip, Config.Locations[store]["blipsprite"])
-                SetBlipScale(StoreBlip, 0.6)
-                SetBlipDisplay(StoreBlip, 4)
-                SetBlipAsShortRange(StoreBlip, true)
-                BeginTextCommandSetBlipName("STRING")
-                AddTextComponentSubstringPlayerName(Config.Locations[store]["label"])
-                EndTextCommandSetBlipName(StoreBlip)
-            end
-        end
-    end
+	for store, _ in pairs(Config.Locations) do
+		if Config.Locations[store]["showblip"] then
+			StoreBlip = AddBlipForCoord(Config.Locations[store]["coords"][1]["x"], Config.Locations[store]["coords"][1]["y"], Config.Locations[store]["coords"][1]["z"])
+			SetBlipColour(StoreBlip, 0)
+
+			if Config.Locations[store]["products"] == Config.Products["normal"] then
+				SetBlipSprite(StoreBlip, 52)
+				SetBlipScale(StoreBlip, 0.6)
+			elseif Config.Locations[store]["products"] == Config.Products["coffeeplace"] then
+				SetBlipSprite(StoreBlip, 52)
+				SetBlipScale(StoreBlip, 0.6)
+			elseif Config.Locations[store]["products"] == Config.Products["gearshop"] then
+				SetBlipSprite(StoreBlip, 52)
+				SetBlipScale(StoreBlip, 0.6)
+			elseif Config.Locations[store]["products"] == Config.Products["hardware"] then
+				SetBlipSprite(StoreBlip, 402)
+				SetBlipScale(StoreBlip, 0.8)
+			elseif Config.Locations[store]["products"] == Config.Products["weapons"] then
+				SetBlipSprite(StoreBlip, 110)
+				SetBlipScale(StoreBlip, 0.85)
+			elseif Config.Locations[store]["products"] == Config.Products["leisureshop"] then
+				SetBlipSprite(StoreBlip, 52)
+				SetBlipScale(StoreBlip, 0.6)
+				SetBlipColour(StoreBlip, 3)
+			elseif Config.Locations[store]["products"] == Config.Products["mustapha"] then
+				SetBlipSprite(StoreBlip, 225)
+				SetBlipScale(StoreBlip, 0.6)
+				SetBlipColour(StoreBlip, 3)
+			elseif Config.Locations[store]["products"] == Config.Products["coffeeshop"] then
+				SetBlipSprite(StoreBlip, 140)
+				SetBlipScale(StoreBlip, 0.55)
+			elseif Config.Locations[store]["products"] == Config.Products["casino"] then
+				SetBlipSprite(StoreBlip, 617)
+				SetBlipScale(StoreBlip, 0.70)
+			end
+
+			SetBlipDisplay(StoreBlip, 4)
+			SetBlipAsShortRange(StoreBlip, true)
+			BeginTextCommandSetBlipName("STRING")
+			AddTextComponentSubstringPlayerName(Config.Locations[store]["label"])
+			EndTextCommandSetBlipName(StoreBlip)
+		end
+	end
 end)
